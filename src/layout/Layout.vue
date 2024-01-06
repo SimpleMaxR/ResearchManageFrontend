8ab51051b9b5bc41e7ead75dc3883b07af4bddfd
Hugo Ho <hugoho912@outlook.com>
2024-01-06 13:35:43 +0800

    [style] 优化背景

--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -1,10 +1,13 @@
 <template>
     <div class="flex flex-col h-screen bg-gray-50">
+      <div class="absolute left-0 top-0 z-5">
+        <div class="relative w-[100vw] h-[150px] -translate-y-1/2 bg-gradient-to-bl from-green-600 to-transparent rounded-full blur-[50px] opacity-50"></div>
+      </div>
         <Header :user="user"
                 class="fixed top-6 left-0 right-0 mx-auto w-[97%] z-10"
             style="border-radius: 12px; backdrop-filter: blur(5px); opacity: 96%"
         />
-        <div class="flex flex-row flex-grow mt-24 bg-gray-50 max-h-[96%] overflow-hidden">
+        <div class="flex flex-row flex-grow mt-24 bg-gray-50 max-h-[96%] overflow-hidden z-1">
             <SideMenu />
             <div class="max-h-full flex flex-col flex-grow bg-gray-50 mx-10 my-6 p-2 overflow-scroll scroll-auto">
                 <RouterView />
